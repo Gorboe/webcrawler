23969414210d440c138a0fbfeed9ef495e2bca76
Gorboe <31539445+Gorboe@users.noreply.github.com>
2020-12-06 12:31:49 +0100

    Fixed the path problems
    now the crawler is crawling every page and downloading it.

--- crawl.py
+++ crawl.py
@@ -1,13 +1,12 @@
 import requests
 import re
 import os
-
+import urllib.parse
 
 
 class Crawl:
     def __init__(self, url, depth, user_regex):
         self.url = url
-        self.unique_list = []
         # self.depth = depth
         # self.user_regex = user_regex
         self.base_domain = self.get_base_domain()
@@ -18,59 +17,53 @@ class Crawl:
         return re.findall(regex, self.url)
 
     def get_path(self, url):
-        regex = "(?:(?:https?:\/\/)?www." + self.base_domain[0] + ".\w+(\/[a-zæøåA-ZÆØÅ0-9?]+)+)"
-        path = re.findall(regex, url)
-        if not path:
-            return ""
-        else:
-            return path[0]
+        return urllib.parse.urlparse(url).path
 
     def crawl(self, url, depth):
         print("Crawling...")
 
         # create folder for this page to store the page itself, and data
-        print("Making dir: " + self.base_domain[0] + self.get_path(url))
-        os.mkdir(self.base_domain[0] + self.get_path(url))
+        try:
+            print("Making dir: " + self.base_domain[0] + self.get_path(url))
+            os.mkdir(self.base_domain[0] + self.get_path(url))
 
-        # download page
-        response = requests.get(url)
-        file = open(self.base_domain[0] + self.get_path(url) + "/page.html", "w")
-        file.write(response.content.decode("utf-8"))
-        file.close()
+            # download page
+            response = requests.get(url)
+            file = open(self.base_domain[0] + self.get_path(url) + "/page.html", "w")
+            file.write(response.content.decode("utf-8"))
+            file.close()
 
-        # do whatever i want to page, find headers, words ect..
-        # check the depth, if 0 return. We don't wanna crawl deeper.
-        if depth == 0:
-            return
+            # do whatever i want to page, find headers, words ect..
+            # check the depth, if 0 return. We don't wanna crawl deeper.
+            if depth == 0:
+                return
 
-        # get links url_list = ....
-        # Find links on the site
-        file = open(self.base_domain[0] + self.get_path(url) + "/page.html", "r")
-        lines = file.read()
-        file.close()
-        links = self.print_all_links(lines)
+            # get links url_list = ....
+            # Find links on the site
+            file = open(self.base_domain[0] + self.get_path(url) + "/page.html", "r")
+            lines = file.read()
+            file.close()
+            links = self.get_all_links(lines)
 
-        # for url in url_list:
-        for link in links:
-            print("recursing: " + link + "\n")
-            self.crawl(link, int(depth)-1)
+            # for url in url_list:
+            for link in links:
+                print("recursing: " + link + "\n")
+                self.crawl(link, int(depth)-1)
+        except:
+            print("Dupe, did not create folder")
 
-    def print_all_links(self, text):
-        domain = "oslomet"  # TEMPORARY
-        regex = "(?:href=\")((?:(?:https?:\/\/(?:www.)?)" + domain + ".\w+)?(?:\/[a-zæøåA-ZÆØÅ0-9?]+)+(?:\/)*)(?:\")"
+    def get_all_links(self, text):
+        regex = "(?:href=\")((?:(?:https?:\/\/(?:www.)?)" + self.base_domain[0] + ".\w+)?(?:\/[a-zæøåA-ZÆØÅ0-9?]+)+(?:\/)*)(?:\")"
         links = re.findall(regex, text)
         return self.format_links(links)
 
     def format_links(self, links):
-        user_link = "https://www.oslomet.no"  # TEMPORARY
-        domain = "oslomet"  # TEMPORARY
         link_list = []
         for link in links:
-            regex = "https?:\/\/www." + domain + ".\w+"
+            regex = "https?:\/\/www." + self.base_domain[0] + ".\w+"
             n = re.findall(regex, link)
             if len(n) == 0:
-                link_list.append(user_link + link)
-
+                link_list.append(self.url + link)
 
         # Remove duplicates
         link_list = list(dict.fromkeys(link_list))
@@ -78,23 +71,11 @@ class Crawl:
         # Optional remove \en\ ?
         updated_link_list = []
         for link in link_list:
-            regex = "https?:\/\/www." + domain + ".\w+\/en\/"
+            regex = "https?:\/\/www." + self.base_domain[0] + ".\w+\/en\/"
             n = re.findall(regex, link)
             if len(n) == 0:
                 updated_link_list.append(link)
-        print("Before:" + str(updated_link_list))
-        # add and check if the new links are already in the list
-        for link in updated_link_list:
-            isUnique = True
-            for url in self.unique_list:
-                if link == url:
-                    isUnique = False
-            if isUnique:
-                self.unique_list.append(link)
-            else:
-                print("remove dupe: " + link)
-                updated_link_list.remove(link)
-        print("After: " + str(updated_link_list))
+
         return updated_link_list
 
 # User inputs
--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import os
 import re
+import urllib.parse
 
 def get_path(url):
     regex = "(?:(?:https?:\/\/)?www." + "oslomet" + ".\w+(\/[a-zæøåA-ZÆØÅ0-9?]+)+)"
     path = re.findall(regex, url)
-    print("THAURL: " + url)
     if not path:
-        print("this runs")
         return ""
     else:
-        print("this: " + path[0])
         return path[0]
 
-print(get_path("https://www.oslomet.no/forskning/blablabla"))
+def path(url):
+    return urllib.parse.urlparse(url).path
+
+print(get_path("https://www.oslomet.no/om/arrangement/arrangementsoversikt"))
+print(path("https://www.oslomet.no/om/arrangement/arrangementsoversikt"))
+print(path("https://www.oslomet.no/"))
